39ea13e040eccfc016a60328a3fcec3e49f6293f
Bruno Martins <brunomart97@hotmail.com>
2024-03-03 11:16:16 -0300

    fix: removing the return key

--- src/helpers/formatLevelData.ts
+++ src/helpers/formatLevelData.ts
@@ -3,6 +3,6 @@ import type { Language, Level } from '../typings/SecretWord'
 export const formatLevelData = (level: Level, language: Language) => {
   return {
     id: level.id,
-    levelData: level.levelData[language]
+    clues: level.levelData[language].clues
   }
 }
