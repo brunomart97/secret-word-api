c7def5f7f3d64649de1d7f0746e97381b4b5ba4f
Bruno Martins <brunomart97@hotmail.com>
2024-03-30 10:23:53 -0300

    chore: level 1 and 2 clues

--- src/data/levels/level2.ts
+++ src/data/levels/level2.ts
@@ -3,24 +3,92 @@ export const level2 = [
     id: '2.1',
     levelData: {
       pt: {
+        key: 'Instrumentos Musicais',
+        clues: [
+          'Violino',
+          'Piano',
+          'Violão',
+          'Guitarra',
+          'Flauta',
+          'Bateria',
+          'Saxofone',
+          'Trompete',
+          'Cello',
+          'Contrabaixo',
+          'Harpa',
+          'Acordeão',
+          'Banjo',
+          'Trombone',
+          'Xilofone',
+          'Oboé'
+        ]
+      },
+      en: {
+        key: 'Musical Instruments',
+        clues: [
+          'Violin',
+          'Piano',
+          'Guitar',
+          'Guitar',
+          'Flute',
+          'Drums',
+          'Saxophone',
+          'Trumpet',
+          'Cello',
+          'Double bass',
+          'Harp',
+          'Accordion',
+          'Banjo',
+          'Trombone',
+          'Xylophone',
+          'Oboe'
+        ]
+      },
+      es: {
+        key: 'Instrumentos Musicales',
+        clues: [
+          'Violín',
+          'Piano',
+          'Guitarra',
+          'Guitarra',
+          'Flauta',
+          'Batería',
+          'Saxofón',
+          'Trompeta',
+          'Violonchelo',
+          'Contrabajo',
+          'Arpa',
+          'Acordeón',
+          'Banjo',
+          'Trombón',
+          'Xilófono',
+          'Oboe'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.2',
+    levelData: {
+      pt: {
         key: 'Estações do Ano',
         clues: [
           'Primavera',
           'Verão',
           'Outono',
           'Inverno',
-          'Equinócio',
           'Solstício',
+          'Equinócio',
+          'Temperatura',
+          'Chuva',
+          'Neve',
+          'Flores',
           'Calor',
           'Frio',
-          'Flores',
-          'Folhas',
-          'Neve',
-          'Chuva',
-          'Soleira',
           'Ventania',
-          'Polar',
-          'Temperatura'
+          'Gelo',
+          'Folhas',
+          'Sol'
         ]
       },
       en: {
@@ -30,18 +98,18 @@ export const level2 = [
           'Summer',
           'Autumn',
           'Winter',
-          'Equinox',
           'Solstice',
+          'Equinox',
+          'Temperature',
+          'Rain',
+          'Snow',
+          'Flowers',
           'Heat',
           'Cold',
-          'Flowers',
-          'Leaves',
-          'Snow',
-          'Rain',
-          'Sunshine',
           'Wind',
-          'Polar',
-          'Temperature'
+          'Ice',
+          'Leaves',
+          'Sun'
         ]
       },
       es: {
@@ -51,86 +119,559 @@ export const level2 = [
           'Verano',
           'Otoño',
           'Invierno',
-          'Equinoccio',
           'Solsticio',
+          'Equinoccio',
+          'Temperatura',
+          'Lluvia',
+          'Nieve',
+          'Flores',
           'Calor',
           'Frío',
-          'Flores',
-          'Hojas',
-          'Nieve',
-          'Lluvia',
-          'Soleada',
           'Viento',
-          'Polar',
-          'Temperatura'
+          'Hielo',
+          'Hojas',
+          'Sol'
         ]
       }
     }
   },
   {
-    id: '2.2',
+    id: '2.3',
     levelData: {
       pt: {
-        key: 'Instrumentos Musicais',
+        key: 'Países',
         clues: [
-          'Violino',
-          'Violão',
-          'Piano',
-          'Flauta',
-          'Guitarra',
-          'Bateria',
-          'Trompete',
-          'Saxofone',
-          'Tambor',
-          'Teclado',
-          'Violoncelo',
-          'Harpa',
-          'Trombone',
-          'Clarinete',
-          'Xilofone',
-          'Acordeão'
+          'Brasil',
+          'Estados Unidos',
+          'China',
+          'Índia',
+          'Rússia',
+          'Canadá',
+          'França',
+          'Alemanha',
+          'Reino Unido',
+          'Japão',
+          'Austrália',
+          'Itália',
+          'México',
+          'Coreia do Sul',
+          'Indonésia',
+          'África do Sul'
         ]
       },
       en: {
-        key: 'Musical Instruments',
+        key: 'Countries',
         clues: [
-          'Violin',
-          'Guitar',
-          'Piano',
-          'Flute',
-          'Electric Guitar',
-          'Drums',
-          'Trumpet',
-          'Saxophone',
-          'Drum',
-          'Keyboard',
-          'Cello',
-          'Harp',
-          'Trombone',
-          'Clarinet',
-          'Xylophone',
-          'Accordion'
+          'Brazil',
+          'United States',
+          'China',
+          'India',
+          'Russia',
+          'Canada',
+          'France',
+          'Germany',
+          'United Kingdom',
+          'Japan',
+          'Australia',
+          'Italy',
+          'Mexico',
+          'South Korea',
+          'Indonesia',
+          'South Africa'
         ]
       },
       es: {
-        key: 'Instrumentos Musicales',
+        key: 'Países',
         clues: [
-          'Violín',
-          'Guitarra',
-          'Piano',
-          'Flauta',
-          'Guitarra',
-          'Batería',
-          'Trompeta',
-          'Saxofón',
-          'Tambor',
-          'Teclado',
-          'Violonchelo',
-          'Arpa',
-          'Trombón',
-          'Clarinete',
-          'Xilófono',
-          'Acordeón'
+          'Brasil',
+          'Estados Unidos',
+          'China',
+          'India',
+          'Rusia',
+          'Canadá',
+          'Francia',
+          'Alemania',
+          'Reino Unido',
+          'Japón',
+          'Australia',
+          'Italia',
+          'México',
+          'Corea del Sur',
+          'Indonesia',
+          'Sudáfrica'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.4',
+    levelData: {
+      pt: {
+        key: 'Alimentos',
+        clues: [
+          'Arroz',
+          'Feijão',
+          'Macarrão',
+          'Pão',
+          'Queijo',
+          'Frango',
+          'Carne',
+          'Peixe',
+          'Ovo',
+          'Batata',
+          'Tomate',
+          'Alface',
+          'Banana',
+          'Maçã',
+          'Laranja',
+          'Morango'
+        ]
+      },
+      en: {
+        key: 'Foods',
+        clues: [
+          'Rice',
+          'Beans',
+          'Pasta',
+          'Bread',
+          'Cheese',
+          'Chicken',
+          'Beef',
+          'Fish',
+          'Egg',
+          'Potato',
+          'Tomato',
+          'Lettuce',
+          'Banana',
+          'Apple',
+          'Orange',
+          'Strawberry'
+        ]
+      },
+      es: {
+        key: 'Alimentos',
+        clues: [
+          'Arroz',
+          'Frijoles',
+          'Pasta',
+          'Pan',
+          'Queso',
+          'Pollo',
+          'Carne',
+          'Pescado',
+          'Huevo',
+          'Papa',
+          'Tomate',
+          'Lechuga',
+          'Plátano',
+          'Manzana',
+          'Naranja',
+          'Fresa'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.5',
+    levelData: {
+      pt: {
+        key: 'Esportes',
+        clues: [
+          'Futebol',
+          'Basquete',
+          'Tênis',
+          'Vôlei',
+          'Natação',
+          'Atletismo',
+          'Ginástica',
+          'Golfe',
+          'Surfe',
+          'Boxe',
+          'Ciclismo',
+          'Rúgbi',
+          'Hóquei',
+          'Handebol',
+          'Escalada',
+          'Esqui'
+        ]
+      },
+      en: {
+        key: 'Sports',
+        clues: [
+          'Soccer',
+          'Basketball',
+          'Tennis',
+          'Volleyball',
+          'Swimming',
+          'Athletics',
+          'Gymnastics',
+          'Golf',
+          'Surfing',
+          'Boxing',
+          'Cycling',
+          'Rugby',
+          'Hockey',
+          'Handball',
+          'Rock climbing',
+          'Skiing'
+        ]
+      },
+      es: {
+        key: 'Deportes',
+        clues: [
+          'Fútbol',
+          'Baloncesto',
+          'Tenis',
+          'Voleibol',
+          'Natación',
+          'Atletismo',
+          'Gimnasia',
+          'Golf',
+          'Surf',
+          'Boxeo',
+          'Ciclismo',
+          'Rugby',
+          'Hockey',
+          'Balonmano',
+          'Escalada en roca',
+          'Esquí'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.6',
+    levelData: {
+      pt: {
+        key: 'Atleta',
+        clues: [
+          'Corredor',
+          'Nadador',
+          'Jogador',
+          'Saltador',
+          'Lutador',
+          'Ciclista',
+          'Arremessador',
+          'Goleiro',
+          'Remador',
+          'Ginasta',
+          'Tenista',
+          'Esquiador',
+          'Cavaleiro',
+          'Surfista',
+          'Atleta',
+          'Maratonista'
+        ]
+      },
+      en: {
+        key: 'Athlete',
+        clues: [
+          'Runner',
+          'Swimmer',
+          'Player',
+          'Jumper',
+          'Fighter',
+          'Cyclist',
+          'Thrower',
+          'Goalkeeper',
+          'Rower',
+          'Gymnast',
+          'Tennis player',
+          'Skier',
+          'Rider',
+          'Surfer',
+          'Athlete',
+          'Marathon runner'
+        ]
+      },
+      es: {
+        key: 'Atleta',
+        clues: [
+          'Corredor',
+          'Nadador',
+          'Jugador',
+          'Saltador',
+          'Luchador',
+          'Ciclista',
+          'Lanzador',
+          'Portero',
+          'Remador',
+          'Gimnasta',
+          'Tenista',
+          'Esquiador',
+          'Jinete',
+          'Surfista',
+          'Atleta',
+          'Maratonista'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.7',
+    levelData: {
+      pt: {
+        key: 'Luta',
+        clues: [
+          'Boxe',
+          'Judô',
+          'Karate',
+          'Jiu-jitsu',
+          'Muay Thai',
+          'Wrestling',
+          'Taekwondo',
+          'Kung Fu',
+          'Sumô',
+          'Capoeira',
+          'Luta livre',
+          'Kickboxing',
+          'Aikido',
+          'Hapkido',
+          'Krav Maga',
+          'Sambo'
+        ]
+      },
+      en: {
+        key: 'Combat Sports',
+        clues: [
+          'Boxing',
+          'Judo',
+          'Karate',
+          'Jiu-jitsu',
+          'Muay Thai',
+          'Wrestling',
+          'Taekwondo',
+          'Kung Fu',
+          'Sumo',
+          'Capoeira',
+          'Freestyle wrestling',
+          'Kickboxing',
+          'Aikido',
+          'Hapkido',
+          'Krav Maga',
+          'Sambo'
+        ]
+      },
+      es: {
+        key: 'Lucha',
+        clues: [
+          'Boxeo',
+          'Judo',
+          'Karate',
+          'Jiu-jitsu',
+          'Muay Thai',
+          'Lucha libre',
+          'Taekwondo',
+          'Kung Fu',
+          'Sumo',
+          'Capoeira',
+          'Lucha grecorromana',
+          'Kickboxing',
+          'Aikido',
+          'Hapkido',
+          'Krav Maga',
+          'Sambo'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.8',
+    levelData: {
+      pt: {
+        key: 'Escola',
+        clues: [
+          'Professor',
+          'Aluno',
+          'Aula',
+          'Lápis',
+          'Caderno',
+          'Livro',
+          'Prova',
+          'Borracha',
+          'Quadro',
+          'Merenda',
+          'Escola',
+          'Educação',
+          'Caneta',
+          'Mochila',
+          'Pátio',
+          'Diretor'
+        ]
+      },
+      en: {
+        key: 'School',
+        clues: [
+          'Teacher',
+          'Student',
+          'Class',
+          'Pencil',
+          'Notebook',
+          'Book',
+          'Exam',
+          'Eraser',
+          'Blackboard',
+          'Snack',
+          'School',
+          'Education',
+          'Pen',
+          'Backpack',
+          'Playground',
+          'Principal'
+        ]
+      },
+      es: {
+        key: 'Escuela',
+        clues: [
+          'Profesor',
+          'Estudiante',
+          'Clase',
+          'Lápiz',
+          'Cuaderno',
+          'Libro',
+          'Examen',
+          'Borrador',
+          'Pizarra',
+          'Merienda',
+          'Escuela',
+          'Educación',
+          'Pluma',
+          'Mochila',
+          'Patio de recreo',
+          'Director'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.9',
+    levelData: {
+      pt: {
+        key: 'Carro',
+        clues: [
+          'Rodas',
+          'Motor',
+          'Volante',
+          'Pneu',
+          'Freio',
+          'Marcha',
+          'Bancos',
+          'Farol',
+          'Cinto',
+          'Retrovisor',
+          'Combustível',
+          'Chave',
+          'Porta',
+          'Ignição',
+          'Óleo',
+          'Para-brisa'
+        ]
+      },
+      en: {
+        key: 'Car',
+        clues: [
+          'Wheels',
+          'Engine',
+          'Steering wheel',
+          'Tire',
+          'Brake',
+          'Gear',
+          'Seats',
+          'Headlight',
+          'Seatbelt',
+          'Rearview mirror',
+          'Fuel',
+          'Key',
+          'Door',
+          'Ignition',
+          'Oil',
+          'Windshield'
+        ]
+      },
+      es: {
+        key: 'Coche',
+        clues: [
+          'Ruedas',
+          'Motor',
+          'Volante',
+          'Neumático',
+          'Freno',
+          'Marcha',
+          'Asientos',
+          'Faros',
+          'Cinturón de seguridad',
+          'Espejo retrovisor',
+          'Combustible',
+          'Llave',
+          'Puerta',
+          'Encendido',
+          'Aceite',
+          'Parabrisas'
+        ]
+      }
+    }
+  },
+  {
+    id: '2.10',
+    levelData: {
+      pt: {
+        key: 'Vidro',
+        clues: [
+          'Janela',
+          'Garrafa',
+          'Copo',
+          'Espelho',
+          'Lente',
+          'Vidraceiro',
+          'Frasco',
+          'Vasculhador',
+          'Cristal',
+          'Taça',
+          'Prato',
+          'Lanterna',
+          'Óculos',
+          'Aquário',
+          'Para-brisa'
+        ]
+      },
+      en: {
+        key: 'Glass',
+        clues: [
+          'Window',
+          'Bottle',
+          'Cup',
+          'Mirror',
+          'Lens',
+          'Glazier',
+          'Jar',
+          'Vial',
+          'Crystal',
+          'Goblet',
+          'Plate',
+          'Lantern',
+          'Glasses',
+          'Aquarium',
+          'Windshield'
+        ]
+      },
+      es: {
+        key: 'Vidrio',
+        clues: [
+          'Ventana',
+          'Botella',
+          'Vaso',
+          'Espejo',
+          'Lente',
+          'Vidriero',
+          'Frasco',
+          'Ampolla',
+          'Cristal',
+          'Copa',
+          'Plato',
+          'Linterna',
+          'Gafas',
+          'Acuario',
+          'Parabrisas'
         ]
       }
     }
