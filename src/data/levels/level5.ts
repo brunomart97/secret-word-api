4f6738c06944670f8e622df69c816eb93b44c176
Bruno Martins <brunomart97@hotmail.com>
2024-03-30 10:58:17 -0300

    chore: level 5 clues

--- src/data/levels/level5.ts
+++ src/data/levels/level5.ts
@@ -3,74 +3,6 @@ export const level5 = [
     id: '5.1',
     levelData: {
       pt: {
-        key: 'Escritores',
-        clues: [
-          'William Shakespeare',
-          'Charles Dickens',
-          'Jane Austen',
-          'Fyodor Dostoevsky',
-          'Gabriel García Márquez',
-          'Leo Tolstoy',
-          'Mark Twain',
-          'Emily Brontë',
-          'Homer',
-          'Virginia Woolf',
-          'J.R.R. Tolkien',
-          'George Orwell',
-          'Hermann Hesse',
-          'Miguel de Cervantes',
-          'Edgar Allan Poe',
-          'Haruki Murakami'
-        ]
-      },
-      en: {
-        key: 'Writers',
-        clues: [
-          'William Shakespeare',
-          'Charles Dickens',
-          'Jane Austen',
-          'Fyodor Dostoevsky',
-          'Gabriel García Márquez',
-          'Leo Tolstoy',
-          'Mark Twain',
-          'Emily Brontë',
-          'Homer',
-          'Virginia Woolf',
-          'J.R.R. Tolkien',
-          'George Orwell',
-          'Hermann Hesse',
-          'Miguel de Cervantes',
-          'Edgar Allan Poe',
-          'Haruki Murakami'
-        ]
-      },
-      es: {
-        key: 'Escritores',
-        clues: [
-          'William Shakespeare',
-          'Charles Dickens',
-          'Jane Austen',
-          'Fyodor Dostoevsky',
-          'Gabriel García Márquez',
-          'Leo Tolstoy',
-          'Mark Twain',
-          'Emily Brontë',
-          'Homero',
-          'Virginia Woolf',
-          'J.R.R. Tolkien',
-          'George Orwell',
-          'Hermann Hesse',
-          'Miguel de Cervantes',
-          'Edgar Allan Poe',
-          'Haruki Murakami'
-        ]
-      }
-    }
-  },
-  {
-    id: '5.2',
-    levelData: {
-      pt: {
         key: 'Pintores',
         clues: [
           'Leonardo da Vinci',
@@ -78,17 +10,17 @@ export const level5 = [
           'Pablo Picasso',
           'Michelangelo',
           'Claude Monet',
-          'Rembrandt',
-          'Edvard Munch',
           'Salvador Dalí',
+          'Rembrandt',
           'Raphael',
+          'Jackson Pollock',
           "Georgia O'Keeffe",
           'Frida Kahlo',
-          'Hieronymus Bosch',
-          'Diego Velázquez',
-          'Gustav Klimt',
+          'Sandro Botticelli',
+          'Edvard Munch',
           'Henri Matisse',
-          'Andy Warhol'
+          'Gustav Klimt',
+          'Diego Velázquez'
         ]
       },
       en: {
@@ -99,17 +31,17 @@ export const level5 = [
           'Pablo Picasso',
           'Michelangelo',
           'Claude Monet',
-          'Rembrandt',
-          'Edvard Munch',
           'Salvador Dalí',
+          'Rembrandt',
           'Raphael',
+          'Jackson Pollock',
           "Georgia O'Keeffe",
           'Frida Kahlo',
-          'Hieronymus Bosch',
-          'Diego Velázquez',
-          'Gustav Klimt',
+          'Sandro Botticelli',
+          'Edvard Munch',
           'Henri Matisse',
-          'Andy Warhol'
+          'Gustav Klimt',
+          'Diego Velázquez'
         ]
       },
       es: {
@@ -118,19 +50,631 @@ export const level5 = [
           'Leonardo da Vinci',
           'Vincent van Gogh',
           'Pablo Picasso',
-          'Miguel Ángel',
+          'Michelangelo',
           'Claude Monet',
-          'Rembrandt',
-          'Edvard Munch',
           'Salvador Dalí',
-          'Rafael',
+          'Rembrandt',
+          'Raphael',
+          'Jackson Pollock',
           "Georgia O'Keeffe",
           'Frida Kahlo',
-          'Hieronymus Bosch',
-          'Diego Velázquez',
-          'Gustav Klimt',
+          'Sandro Botticelli',
+          'Edvard Munch',
           'Henri Matisse',
-          'Andy Warhol'
+          'Gustav Klimt',
+          'Diego Velázquez'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.2',
+    levelData: {
+      pt: {
+        key: 'Video Game',
+        clues: [
+          'Console',
+          'Controle',
+          'Jogador',
+          'Personagem',
+          'Aventura',
+          'Gráfico',
+          'Plataforma',
+          'Missão',
+          'Nível',
+          'Multiplayer',
+          'Fase',
+          'Power-up',
+          'Estratégia',
+          'Arcade',
+          'História',
+          'Desafio'
+        ]
+      },
+      en: {
+        key: 'Video Game',
+        clues: [
+          'Console',
+          'Controller',
+          'Player',
+          'Character',
+          'Adventure',
+          'Graphics',
+          'Platform',
+          'Mission',
+          'Level',
+          'Multiplayer',
+          'Stage',
+          'Power-up',
+          'Strategy',
+          'Arcade',
+          'Story',
+          'Challenge'
+        ]
+      },
+      es: {
+        key: 'Videojuego',
+        clues: [
+          'Consola',
+          'Controlador',
+          'Jugador',
+          'Personaje',
+          'Aventura',
+          'Gráficos',
+          'Plataforma',
+          'Misión',
+          'Nivel',
+          'Multijugador',
+          'Fase',
+          'Power-up',
+          'Estrategia',
+          'Arcade',
+          'Historia',
+          'Desafío'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.3',
+    levelData: {
+      pt: {
+        key: 'Jornada',
+        clues: [
+          'Viagem',
+          'Aventura',
+          'Desafio',
+          'Exploração',
+          'Experiência',
+          'Caminho',
+          'Destino',
+          'Superar',
+          'Conquista',
+          'Transformação',
+          'Aprendizado',
+          'Crescimento',
+          'Obstáculos',
+          'Triunfo',
+          'Descoberta',
+          'Autoconhecimento'
+        ]
+      },
+      en: {
+        key: 'Journey',
+        clues: [
+          'Travel',
+          'Adventure',
+          'Challenge',
+          'Exploration',
+          'Experience',
+          'Path',
+          'Destination',
+          'Overcome',
+          'Achievement',
+          'Transformation',
+          'Learning',
+          'Growth',
+          'Obstacles',
+          'Triumph',
+          'Discovery',
+          'Self-discovery'
+        ]
+      },
+      es: {
+        key: 'Jornada',
+        clues: [
+          'Viaje',
+          'Aventura',
+          'Desafío',
+          'Exploración',
+          'Experiencia',
+          'Camino',
+          'Destino',
+          'Superar',
+          'Logro',
+          'Transformación',
+          'Aprendizaje',
+          'Crecimiento',
+          'Obstáculos',
+          'Triunfo',
+          'Descubrimiento',
+          'Autoconocimiento'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.4',
+    levelData: {
+      pt: {
+        key: 'Lenda',
+        clues: [
+          'Mito',
+          'Tradição',
+          'História',
+          'Fantasia',
+          'Herói',
+          'Narrativa',
+          'Mistério',
+          'Folclore',
+          'Personagem',
+          'Magia',
+          'Aventura',
+          'Mitologia',
+          'Fábula',
+          'Conto',
+          'Encantamento',
+          'Crença'
+        ]
+      },
+      en: {
+        key: 'Legend',
+        clues: [
+          'Myth',
+          'Tradition',
+          'Story',
+          'Fantasy',
+          'Hero',
+          'Narrative',
+          'Mystery',
+          'Folklore',
+          'Character',
+          'Magic',
+          'Adventure',
+          'Mythology',
+          'Fable',
+          'Tale',
+          'Enchantment',
+          'Belief'
+        ]
+      },
+      es: {
+        key: 'Leyenda',
+        clues: [
+          'Mito',
+          'Tradición',
+          'Historia',
+          'Fantasía',
+          'Héroe',
+          'Narrativa',
+          'Misterio',
+          'Folclore',
+          'Personaje',
+          'Magia',
+          'Aventura',
+          'Mitología',
+          'Fábula',
+          'Cuento',
+          'Encantamiento',
+          'Creencia'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.5',
+    levelData: {
+      pt: {
+        key: 'Faculdade',
+        clues: [
+          'Educação',
+          'Ensino',
+          'Aprendizado',
+          'Graduação',
+          'Universidade',
+          'Aluno',
+          'Professor',
+          'Disciplina',
+          'Curso',
+          'Bacharelado',
+          'Mestrado',
+          'Doutorado',
+          'Pesquisa',
+          'Conhecimento',
+          'Trabalho em equipe',
+          'Desafio'
+        ]
+      },
+      en: {
+        key: 'College',
+        clues: [
+          'Education',
+          'Teaching',
+          'Learning',
+          'Graduation',
+          'University',
+          'Student',
+          'Professor',
+          'Subject',
+          'Course',
+          "Bachelor's degree",
+          "Master's degree",
+          'Doctorate',
+          'Research',
+          'Knowledge',
+          'Teamwork',
+          'Challenge'
+        ]
+      },
+      es: {
+        key: 'Universidad',
+        clues: [
+          'Educación',
+          'Enseñanza',
+          'Aprendizaje',
+          'Graduación',
+          'Universidad',
+          'Estudiante',
+          'Profesor',
+          'Asignatura',
+          'Curso',
+          'Licenciatura',
+          'Maestría',
+          'Doctorado',
+          'Investigación',
+          'Conocimiento',
+          'Trabajo en equipo',
+          'Desafío'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.6',
+    levelData: {
+      pt: {
+        key: 'Guerra',
+        clues: [
+          'Conflito',
+          'Batalha',
+          'Violência',
+          'Exército',
+          'Soldado',
+          'Armas',
+          'Combatente',
+          'Militar',
+          'Estratégia',
+          'Aliança',
+          'Fronteira',
+          'Tática',
+          'Ofensiva',
+          'Defensiva',
+          'Trégua',
+          'Conquista'
+        ]
+      },
+      en: {
+        key: 'War',
+        clues: [
+          'Conflict',
+          'Battle',
+          'Violence',
+          'Army',
+          'Soldier',
+          'Weapons',
+          'Combatant',
+          'Military',
+          'Strategy',
+          'Alliance',
+          'Border',
+          'Tactics',
+          'Offensive',
+          'Defensive',
+          'Truce',
+          'Conquest'
+        ]
+      },
+      es: {
+        key: 'Guerra',
+        clues: [
+          'Conflicto',
+          'Batalla',
+          'Violencia',
+          'Ejército',
+          'Soldado',
+          'Armas',
+          'Combatiente',
+          'Militar',
+          'Estrategia',
+          'Alianza',
+          'Frontera',
+          'Táctica',
+          'Ofensiva',
+          'Defensiva',
+          'Tregua',
+          'Conquista'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.7',
+    levelData: {
+      pt: {
+        key: 'Fantasma',
+        clues: [
+          'Assombração',
+          'Alma penada',
+          'Espírito',
+          'Medo',
+          'Sobrenatural',
+          'Aparição',
+          'Fantasmagórico',
+          'Supernatural',
+          'Mistério',
+          'Terror',
+          'Ectoplasma',
+          'Cemitério',
+          'Casa mal-assombrada',
+          'Espectral',
+          'Ethereal',
+          'Espírito errante'
+        ]
+      },
+      en: {
+        key: 'Ghost',
+        clues: [
+          'Haunting',
+          'Specter',
+          'Spirit',
+          'Fear',
+          'Supernatural',
+          'Ghostly',
+          'Phantom',
+          'Mystery',
+          'Terror',
+          'Ectoplasm',
+          'Graveyard',
+          'Haunted house',
+          'Spectral',
+          'Ethereal',
+          'Wandering spirit',
+          'Apparition'
+        ]
+      },
+      es: {
+        key: 'Fantasma',
+        clues: [
+          'Aparición',
+          'Espectro',
+          'Espíritu',
+          'Miedo',
+          'Sobrenatural',
+          'Fantasmal',
+          'Misterio',
+          'Terror',
+          'Ectoplasma',
+          'Cementerio',
+          'Casa encantada',
+          'Espectral',
+          'Espíritu errante',
+          'Sobrenatural',
+          'Fantasmagórico',
+          'Aparición'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.8',
+    levelData: {
+      pt: {
+        key: 'Foguete',
+        clues: [
+          'Espaço',
+          'Astronáutica',
+          'Viagem',
+          'Propulsão',
+          'Aerodinâmica',
+          'Impulso',
+          'Satélite',
+          'Missão',
+          'Lançamento',
+          'Estágio',
+          'Órbita',
+          'Cosmonauta',
+          'Exploração espacial',
+          'Agência espacial',
+          'Astronomia',
+          'Telescópio'
+        ]
+      },
+      en: {
+        key: 'Rocket',
+        clues: [
+          'Space',
+          'Astronautics',
+          'Journey',
+          'Propulsion',
+          'Aerodynamics',
+          'Thrust',
+          'Satellite',
+          'Mission',
+          'Launch',
+          'Stage',
+          'Orbit',
+          'Cosmonaut',
+          'Space exploration',
+          'Space agency',
+          'Astronomy',
+          'Telescope'
+        ]
+      },
+      es: {
+        key: 'Cohete',
+        clues: [
+          'Espacio',
+          'Astronáutica',
+          'Viaje',
+          'Propulsión',
+          'Aerodinámica',
+          'Impulso',
+          'Satélite',
+          'Misión',
+          'Lanzamiento',
+          'Etapa',
+          'Órbita',
+          'Cosmonauta',
+          'Exploración espacial',
+          'Agencia espacial',
+          'Astronomía',
+          'Telescopio'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.9',
+    levelData: {
+      pt: {
+        key: 'Vento',
+        clues: [
+          'Ar',
+          'Brisa',
+          'Clima',
+          'Corrente',
+          'Sopro',
+          'Frescor',
+          'Movimento',
+          'Natureza',
+          'Rajada',
+          'Suave',
+          'Tempestade',
+          'Ventania',
+          'Zéfiro',
+          'Meteorologia',
+          'Atmosfera',
+          'Efeito'
+        ]
+      },
+      en: {
+        key: 'Wind',
+        clues: [
+          'Air',
+          'Breeze',
+          'Weather',
+          'Current',
+          'Gust',
+          'Freshness',
+          'Movement',
+          'Nature',
+          'Blast',
+          'Gentle',
+          'Storm',
+          'Gale',
+          'Zephyr',
+          'Meteorology',
+          'Atmosphere',
+          'Effect'
+        ]
+      },
+      es: {
+        key: 'Viento',
+        clues: [
+          'Aire',
+          'Brisa',
+          'Clima',
+          'Corriente',
+          'Ráfaga',
+          'Frescura',
+          'Movimiento',
+          'Naturaleza',
+          'Soplo',
+          'Suave',
+          'Tormenta',
+          'Vendaval',
+          'Céfiro',
+          'Meteorología',
+          'Atmósfera',
+          'Efecto'
+        ]
+      }
+    }
+  },
+  {
+    id: '5.10',
+    levelData: {
+      pt: {
+        key: 'Metrópoles',
+        clues: [
+          'Cidade',
+          'Urbano',
+          'População',
+          'Arranha-céu',
+          'Tráfego',
+          'Cultura',
+          'Diversidade',
+          'Movimento',
+          'Centro',
+          'Cosmopolita',
+          'Modernidade',
+          'Economia',
+          'Globalização',
+          'Vida noturna',
+          'Turismo',
+          'Desenvolvimento'
+        ]
+      },
+      en: {
+        key: 'Metropolises',
+        clues: [
+          'City',
+          'Urban',
+          'Population',
+          'Skyscraper',
+          'Traffic',
+          'Culture',
+          'Diversity',
+          'Movement',
+          'Downtown',
+          'Cosmopolitan',
+          'Modernity',
+          'Economy',
+          'Globalization',
+          'Nightlife',
+          'Tourism',
+          'Development'
+        ]
+      },
+      es: {
+        key: 'Metrópolis',
+        clues: [
+          'Ciudad',
+          'Urbano',
+          'Población',
+          'Rascacielos',
+          'Tráfico',
+          'Cultura',
+          'Diversidad',
+          'Movimiento',
+          'Centro',
+          'Cosmopolita',
+          'Modernidad',
+          'Economía',
+          'Globalización',
+          'Vida nocturna',
+          'Turismo',
+          'Desarrollo'
         ]
       }
     }
