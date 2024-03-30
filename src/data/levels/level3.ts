7f002da46faf2634c80e3c8167e1f00cf6bdeec3
Bruno Martins <brunomart97@hotmail.com>
2024-03-30 10:33:31 -0300

    chore: level 3 clues

--- src/data/levels/level3.ts
+++ src/data/levels/level3.ts
@@ -3,66 +3,66 @@ export const level3 = [
     id: '3.1',
     levelData: {
       pt: {
-        key: 'Países',
+        key: 'Astronauta',
         clues: [
-          'Brasil',
-          'Estados Unidos',
-          'China',
-          'Índia',
-          'Rússia',
-          'Japão',
-          'Alemanha',
-          'Indonésia',
-          'México',
-          'França',
-          'Reino Unido',
-          'Itália',
-          'Coreia do Sul',
-          'Arábia Saudita',
-          'Canadá',
-          'Austrália'
+          'Espaço',
+          'NASA',
+          'Lua',
+          'Estrela',
+          'Missão',
+          'Nave',
+          'Cosmonauta',
+          'Exploração',
+          'Gravidade',
+          'Planeta',
+          'Foguete',
+          'Órbita',
+          'Estação espacial',
+          'Astronomia',
+          'Explorador',
+          'Viagem'
         ]
       },
       en: {
-        key: 'Countries',
+        key: 'Astronaut',
         clues: [
-          'Brazil',
-          'United States',
-          'China',
-          'India',
-          'Russia',
-          'Japan',
-          'Germany',
-          'Indonesia',
-          'Mexico',
-          'France',
-          'United Kingdom',
-          'Italy',
-          'South Korea',
-          'Saudi Arabia',
-          'Canada',
-          'Australia'
+          'Space',
+          'NASA',
+          'Moon',
+          'Star',
+          'Mission',
+          'Spaceship',
+          'Cosmonaut',
+          'Exploration',
+          'Gravity',
+          'Planet',
+          'Rocket',
+          'Orbit',
+          'Space station',
+          'Astronomy',
+          'Explorer',
+          'Journey'
         ]
       },
       es: {
-        key: 'Países',
+        key: 'Astronauta',
         clues: [
-          'Brasil',
-          'Estados Unidos',
-          'China',
-          'India',
-          'Rusia',
-          'Japón',
-          'Alemania',
-          'Indonesia',
-          'México',
-          'Francia',
-          'Reino Unido',
-          'Italia',
-          'Corea del Sur',
-          'Arabia Saudita',
-          'Canadá',
-          'Australia'
+          'Espacio',
+          'NASA',
+          'Luna',
+          'Estrella',
+          'Misión',
+          'Nave espacial',
+          'Cosmonauta',
+          'Exploración',
+          'Gravedad',
+          'Planeta',
+          'Cohete',
+          'Órbita',
+          'Estación espacial',
+          'Astronomía',
+          'Explorador',
+          'Viaje'
         ]
       }
     }
@@ -71,66 +71,608 @@ export const level3 = [
     id: '3.2',
     levelData: {
       pt: {
-        key: 'Filósofos',
+        key: 'Cientista',
         clues: [
-          'Sócrates',
-          'Platão',
-          'Aristóteles',
-          'Descartes',
-          'Kant',
-          'Nietzsche',
-          'Sartre',
-          'Hume',
-          'Spinoza',
-          'Locke',
-          'Hegel',
-          'Schopenhauer',
-          'Russell',
-          'Confúcio',
-          'Epicuro',
-          'Foucault'
+          'Laboratório',
+          'Experimento',
+          'Descoberta',
+          'Pesquisa',
+          'Teoria',
+          'Hipótese',
+          'Invenção',
+          'Ciência',
+          'Einstein',
+          'Galileu',
+          'Newton',
+          'Biólogo',
+          'Químico',
+          'Físico',
+          'Genética',
+          'Microscópio'
         ]
       },
       en: {
-        key: 'Philosophers',
+        key: 'Scientist',
         clues: [
-          'Socrates',
-          'Plato',
-          'Aristotle',
-          'Descartes',
-          'Kant',
-          'Nietzsche',
-          'Sartre',
-          'Hume',
-          'Spinoza',
-          'Locke',
-          'Hegel',
-          'Schopenhauer',
-          'Russell',
-          'Confucius',
-          'Epicurus',
-          'Foucault'
+          'Laboratory',
+          'Experiment',
+          'Discovery',
+          'Research',
+          'Theory',
+          'Hypothesis',
+          'Invention',
+          'Science',
+          'Einstein',
+          'Galileo',
+          'Newton',
+          'Biologist',
+          'Chemist',
+          'Physicist',
+          'Genetics',
+          'Microscope'
         ]
       },
       es: {
-        key: 'Filósofos',
+        key: 'Científico',
         clues: [
-          'Sócrates',
-          'Platón',
-          'Aristóteles',
-          'Descartes',
-          'Kant',
-          'Nietzsche',
-          'Sartre',
-          'Hume',
-          'Spinoza',
-          'Locke',
-          'Hegel',
-          'Schopenhauer',
-          'Russell',
-          'Confucio',
-          'Epicuro',
-          'Foucault'
+          'Laboratorio',
+          'Experimento',
+          'Descubrimiento',
+          'Investigación',
+          'Teoría',
+          'Hipótesis',
+          'Invento',
+          'Ciencia',
+          'Einstein',
+          'Galileo',
+          'Newton',
+          'Biólogo',
+          'Químico',
+          'Físico',
+          'Genética',
+          'Microscopio'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.3',
+    levelData: {
+      pt: {
+        key: 'Aventura',
+        clues: [
+          'Exploração',
+          'Coragem',
+          'Desafio',
+          'Descoberta',
+          'Emoção',
+          'Ação',
+          'Viagem',
+          'Aventura',
+          'Destemor',
+          'Explorador',
+          'Mapa',
+          'Jornada',
+          'Expedição',
+          'Tesouro',
+          'Selva',
+          'Mistério'
+        ]
+      },
+      en: {
+        key: 'Adventure',
+        clues: [
+          'Exploration',
+          'Courage',
+          'Challenge',
+          'Discovery',
+          'Excitement',
+          'Action',
+          'Journey',
+          'Adventure',
+          'Fearlessness',
+          'Explorer',
+          'Map',
+          'Expedition',
+          'Treasure',
+          'Jungle',
+          'Mystery'
+        ]
+      },
+      es: {
+        key: 'Aventura',
+        clues: [
+          'Exploración',
+          'Coraje',
+          'Desafío',
+          'Descubrimiento',
+          'Emoción',
+          'Acción',
+          'Viaje',
+          'Aventura',
+          'Intrépido',
+          'Explorador',
+          'Mapa',
+          'Expedición',
+          'Tesoro',
+          'Selva',
+          'Misterio'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.4',
+    levelData: {
+      pt: {
+        key: 'Mistério',
+        clues: [
+          'Enigma',
+          'Suspense',
+          'Investigação',
+          'Segredo',
+          'Assassino',
+          'Crime',
+          'Detetive',
+          'Pista',
+          'Solução',
+          'Revelação',
+          'Curiosidade',
+          'Labirinto',
+          'Sombra',
+          'Trama',
+          'Intriga',
+          'Quebra-cabeça'
+        ]
+      },
+      en: {
+        key: 'Mystery',
+        clues: [
+          'Enigma',
+          'Suspense',
+          'Investigation',
+          'Secret',
+          'Killer',
+          'Crime',
+          'Detective',
+          'Clue',
+          'Solution',
+          'Revelation',
+          'Curiosity',
+          'Labyrinth',
+          'Shadow',
+          'Plot',
+          'Intrigue',
+          'Puzzle'
+        ]
+      },
+      es: {
+        key: 'Misterio',
+        clues: [
+          'Enigma',
+          'Suspense',
+          'Investigación',
+          'Secreto',
+          'Asesino',
+          'Crimen',
+          'Detective',
+          'Pista',
+          'Solución',
+          'Revelación',
+          'Curiosidad',
+          'Laberinto',
+          'Sombra',
+          'Trama',
+          'Intriga',
+          'Rompecabezas'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.5',
+    levelData: {
+      pt: {
+        key: 'Tecnologia',
+        clues: [
+          'Inovação',
+          'Computador',
+          'Internet',
+          'Código',
+          'Software',
+          'Hardware',
+          'Gadget',
+          'Rede',
+          'Celular',
+          'Aplicativo',
+          'Robô',
+          'Inteligência artificial',
+          'Programação',
+          'Digital',
+          'Tecnologia',
+          'Eletrônico'
+        ]
+      },
+      en: {
+        key: 'Technology',
+        clues: [
+          'Innovation',
+          'Computer',
+          'Internet',
+          'Code',
+          'Software',
+          'Hardware',
+          'Gadget',
+          'Network',
+          'Cellphone',
+          'App',
+          'Robot',
+          'Artificial intelligence',
+          'Programming',
+          'Digital',
+          'Technology',
+          'Electronic'
+        ]
+      },
+      es: {
+        key: 'Tecnología',
+        clues: [
+          'Innovación',
+          'Computadora',
+          'Internet',
+          'Código',
+          'Software',
+          'Hardware',
+          'Gadget',
+          'Red',
+          'Teléfono celular',
+          'Aplicación',
+          'Robot',
+          'Inteligencia artificial',
+          'Programación',
+          'Digital',
+          'Tecnología',
+          'Electrónico'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.6',
+    levelData: {
+      pt: {
+        key: 'Herói',
+        clues: [
+          'Coragem',
+          'Altruísmo',
+          'Justiça',
+          'Salvador',
+          'Valentia',
+          'Inspiração',
+          'Lenda',
+          'Superpoderes',
+          'Defensor',
+          'Destemor',
+          'Bravura',
+          'Heroísmo',
+          'Máscara',
+          'Missão',
+          'Liderança',
+          'Resgate'
+        ]
+      },
+      en: {
+        key: 'Hero',
+        clues: [
+          'Courage',
+          'Altruism',
+          'Justice',
+          'Savior',
+          'Valiance',
+          'Inspiration',
+          'Legend',
+          'Superpowers',
+          'Defender',
+          'Fearlessness',
+          'Bravery',
+          'Heroism',
+          'Mask',
+          'Mission',
+          'Leadership',
+          'Rescue'
+        ]
+      },
+      es: {
+        key: 'Héroe',
+        clues: [
+          'Coraje',
+          'Altruismo',
+          'Justicia',
+          'Salvador',
+          'Valentía',
+          'Inspiración',
+          'Leyenda',
+          'Superpoderes',
+          'Defensor',
+          'Intrepidez',
+          'Bravura',
+          'Heroísmo',
+          'Máscara',
+          'Misión',
+          'Liderazgo',
+          'Rescate'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.7',
+    levelData: {
+      pt: {
+        key: 'Monstro',
+        clues: [
+          'Assustador',
+          'Criatura',
+          'Medo',
+          'Fera',
+          'Pesadelo',
+          'Lenda',
+          'Terror',
+          'Monstruosidade',
+          'Abominável',
+          'Gigante',
+          'Horror',
+          'Sombrio',
+          'Mitologia',
+          'Besta',
+          'Ameaça',
+          'Fantasia'
+        ]
+      },
+      en: {
+        key: 'Monster',
+        clues: [
+          'Scary',
+          'Creature',
+          'Fear',
+          'Beast',
+          'Nightmare',
+          'Legend',
+          'Terror',
+          'Monstrosity',
+          'Abominable',
+          'Giant',
+          'Horror',
+          'Dark',
+          'Mythology',
+          'Fiend',
+          'Threat',
+          'Fantasy'
+        ]
+      },
+      es: {
+        key: 'Monstruo',
+        clues: [
+          'Aterrador',
+          'Criatura',
+          'Miedo',
+          'Bestia',
+          'Pesadilla',
+          'Leyenda',
+          'Terror',
+          'Monstruosidad',
+          'Abominable',
+          'Gigante',
+          'Horror',
+          'Sombrio',
+          'Mitología',
+          'Bestia',
+          'Amenaza',
+          'Fantasía'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.8',
+    levelData: {
+      pt: {
+        key: 'Fantasia',
+        clues: [
+          'Imaginação',
+          'Magia',
+          'Encantamento',
+          'Mistério',
+          'Aventura',
+          'Criatividade',
+          'Fada',
+          'Dragão',
+          'Feitiço',
+          'Heroína',
+          'Mundo mágico',
+          'Mitologia',
+          'Sonho',
+          'Unicórnio',
+          'Conto de fadas',
+          'Sereia'
+        ]
+      },
+      en: {
+        key: 'Fantasy',
+        clues: [
+          'Imagination',
+          'Magic',
+          'Enchantment',
+          'Mystery',
+          'Adventure',
+          'Creativity',
+          'Fairy',
+          'Dragon',
+          'Spell',
+          'Heroine',
+          'Magical world',
+          'Mythology',
+          'Dream',
+          'Unicorn',
+          'Fairy tale',
+          'Mermaid'
+        ]
+      },
+      es: {
+        key: 'Fantasía',
+        clues: [
+          'Imaginación',
+          'Magia',
+          'Encantamiento',
+          'Misterio',
+          'Aventura',
+          'Creatividad',
+          'Hada',
+          'Dragón',
+          'Hechizo',
+          'Heroína',
+          'Mundo mágico',
+          'Mitología',
+          'Sueño',
+          'Unicornio',
+          'Cuento de hadas',
+          'Sirena'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.9',
+    levelData: {
+      pt: {
+        key: 'Pirata',
+        clues: [
+          'Tesouro',
+          'Navio',
+          'Ilha',
+          'Mapa',
+          'Corsário',
+          'Saque',
+          'Mar',
+          'Aventura',
+          'Caveira',
+          'Cruz',
+          'Navegação',
+          'Bandana',
+          'Baú',
+          'Maresia',
+          'Ouro',
+          'Bússola'
+        ]
+      },
+      en: {
+        key: 'Pirate',
+        clues: [
+          'Treasure',
+          'Ship',
+          'Island',
+          'Map',
+          'Privateer',
+          'Plunder',
+          'Sea',
+          'Adventure',
+          'Skull',
+          'Crossbones',
+          'Navigation',
+          'Bandana',
+          'Chest',
+          'Salt air',
+          'Gold',
+          'Compass'
+        ]
+      },
+      es: {
+        key: 'Pirata',
+        clues: [
+          'Tesoro',
+          'Barco',
+          'Isla',
+          'Mapa',
+          'Corsario',
+          'Botín',
+          'Mar',
+          'Aventura',
+          'Calavera',
+          'Huesos cruzados',
+          'Navegación',
+          'Bandana',
+          'Cofre',
+          'Aire salado',
+          'Oro',
+          'Brújula'
+        ]
+      }
+    }
+  },
+  {
+    id: '3.10',
+    levelData: {
+      pt: {
+        key: 'Céu',
+        clues: [
+          'Estrela',
+          'Lua',
+          'Sol',
+          'Nuvem',
+          'Astro',
+          'Universo',
+          'Constelação',
+          'Brilho',
+          'Horizonte',
+          'Galáxia',
+          'Astronomia',
+          'Eclipse',
+          'Planeta',
+          'Espaço',
+          'Noite',
+          'Dia'
+        ]
+      },
+      en: {
+        key: 'Sky',
+        clues: [
+          'Star',
+          'Moon',
+          'Sun',
+          'Cloud',
+          'Celestial body',
+          'Universe',
+          'Constellation',
+          'Brightness',
+          'Horizon',
+          'Galaxy',
+          'Astronomy',
+          'Eclipse',
+          'Planet',
+          'Space',
+          'Night',
+          'Day'
+        ]
+      },
+      es: {
+        key: 'Cielo',
+        clues: [
+          'Estrella',
+          'Luna',
+          'Sol',
+          'Nube',
+          'Cuerpo celestial',
+          'Universo',
+          'Constelación',
+          'Brillo',
+          'Horizonte',
+          'Galaxia',
+          'Astronomía',
+          'Eclipse',
+          'Planeta',
+          'Espacio',
+          'Noche',
+          'Día'
         ]
       }
     }
