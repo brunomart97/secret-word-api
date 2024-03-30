03cfcd0deb8e5220351a5bc2adefc1bd3064c193
Bruno Martins <brunomart97@hotmail.com>
2024-03-30 09:02:43 -0300

    chore: level 1 secret words

--- src/data/levels/level1.ts
+++ src/data/levels/level1.ts
@@ -5,64 +5,64 @@ export const level1 = [
       pt: {
         key: 'Cores',
         clues: [
-          'Vermelho',
-          'Azul',
-          'Amarelo',
-          'Verde',
-          'Branco',
+          'Tecido',
+          'Matiz',
           'Preto',
-          'Rosa',
-          'Laranja',
-          'Roxo',
-          'Marrom',
-          'Cinza',
-          'Dourado',
-          'Prateado',
-          'Turquesa',
-          'Bege',
-          'Coral'
+          'Arco-íris',
+          'Branco',
+          'Céu',
+          'Pintura',
+          'Azul',
+          'Verde',
+          'Pigmento',
+          'Vermelho',
+          'Tonalidade',
+          'Amarelo',
+          'Coral',
+          'Tinta',
+          'Marrom'
         ]
       },
       en: {
         key: 'Colors',
         clues: [
-          'Red',
-          'Blue',
-          'Yellow',
-          'Green',
-          'White',
+          'Fabric',
+          'Hue',
           'Black',
-          'Pink',
-          'Orange',
-          'Purple',
-          'Brown',
-          'Gray',
-          'Gold',
-          'Silver',
-          'Turquoise',
-          'Beige',
-          'Coral'
+          'Rainbow',
+          'White',
+          'Sky',
+          'Painting',
+          'Blue',
+          'Green',
+          'Pigment',
+          'Red',
+          'Shade',
+          'Yellow',
+          'Coral',
+          'Ink',
+          'Brown'
         ]
       },
       es: {
         key: 'Colores',
         clues: [
-          'Rojo',
-          'Azul',
-          'Amarillo',
-          'Verde',
-          'Blanco',
+          'Tela',
+          'Matiz',
           'Negro',
-          'Rosa',
-          'Naranja',
-          'Morado',
-          'Marrón',
-          'Gris',
-          'Dorado',
-          'Plateado',
-          'Turquesa',
-          'Beige',
-          'Coral'
+          'Arcoíris',
+          'Blanco',
+          'Cielo',
+          'Pintura',
+          'Azul',
+          'Verde',
+          'Pigmento',
+          'Rojo',
+          'Sombreado',
+          'Amarillo',
+          'Coral',
+          'Tinta',
+          'Marrón'
         ]
       }
     }
@@ -73,64 +73,608 @@ export const level1 = [
       pt: {
         key: 'Frutas',
         clues: [
+          'Suco',
+          'Vitamina',
           'Maçã',
-          'Banana',
-          'Laranja',
+          'Colheita',
+          'Árvore',
+          'Doce',
           'Uva',
-          'Morango',
-          'Abacaxi',
+          'Verão',
+          'Cítrico',
+          'Salada',
+          'Casca',
+          'Nutrição',
           'Limão',
-          'Pera',
-          'Melancia',
+          'Refrescante',
           'Pêssego',
-          'Manga',
-          'Kiwi',
-          'Cereja',
-          'Melão',
-          'Framboesa',
-          'Amora'
+          'Tropical'
         ]
       },
       en: {
         key: 'Fruits',
         clues: [
+          'Juice',
+          'Vitamin',
           'Apple',
-          'Banana',
-          'Orange',
+          'Harvest',
+          'Tree',
+          'Sweet',
           'Grape',
-          'Strawberry',
-          'Pineapple',
+          'Summer',
+          'Citrus',
+          'Salad',
+          'Peel',
+          'Nutrition',
           'Lemon',
-          'Pear',
-          'Watermelon',
+          'Refreshing',
           'Peach',
-          'Mango',
-          'Kiwi',
-          'Cherry',
-          'Melon',
-          'Raspberry',
-          'Blackberry'
+          'Tropical'
         ]
       },
       es: {
         key: 'Frutas',
         clues: [
+          'Jugo',
+          'Vitamina',
           'Manzana',
-          'Plátano',
-          'Naranja',
+          'Cosecha',
+          'Árbol',
+          'Dulce',
           'Uva',
-          'Fresa',
-          'Piña',
+          'Verano',
+          'Cítrico',
+          'Ensalada',
+          'Cáscara',
+          'Nutrición',
           'Limón',
-          'Pera',
-          'Sandía',
+          'Refrescante',
           'Durazno',
-          'Mango',
-          'Kiwi',
-          'Cereza',
-          'Melón',
-          'Frambuesa',
-          'Mora'
+          'Tropical'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.3',
+    levelData: {
+      pt: {
+        key: 'Animais',
+        clues: [
+          'Felino',
+          'Canino',
+          'Gato',
+          'Ave',
+          'Doméstico',
+          'Tigre',
+          'Mamífero',
+          'Pato',
+          'Hamster',
+          'Domesticado',
+          'Pássaro',
+          'Salvagem',
+          'Reptil',
+          'Roedor',
+          'Rato',
+          'Cachorro'
+        ]
+      },
+      en: {
+        key: 'Animals',
+        clues: [
+          'Feline',
+          'Canine',
+          'Cat',
+          'Bird',
+          'Domestic',
+          'Tiger',
+          'Mammal',
+          'Duck',
+          'Hamster',
+          'Domesticated',
+          'Bird',
+          'Wild',
+          'Reptile',
+          'Rodent',
+          'Rat',
+          'Dog'
+        ]
+      },
+      es: {
+        key: 'Animales',
+        clues: [
+          'Felino',
+          'Canino',
+          'Gato',
+          'Ave',
+          'Doméstico',
+          'Tigre',
+          'Mamífero',
+          'Pato',
+          'Hámster',
+          'Domesticado',
+          'Pájaro',
+          'Salvaje',
+          'Reptil',
+          'Roedor',
+          'Rata',
+          'Perro'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.4',
+    levelData: {
+      pt: {
+        key: 'Profissão',
+        clues: [
+          'Trabalho',
+          'Emprego',
+          'Ofício',
+          'Salário',
+          'Médico',
+          'Professor',
+          'Advogado',
+          'Engenheiro',
+          'Carpinteiro',
+          'Pintor',
+          'Jardineiro',
+          'Ator',
+          'Policial',
+          'Bombeiro',
+          'Padeiro',
+          'Eletricista'
+        ]
+      },
+      en: {
+        key: 'Occupation',
+        clues: [
+          'Work',
+          'Job',
+          'Trade',
+          'Salary',
+          'Doctor',
+          'Teacher',
+          'Lawyer',
+          'Engineer',
+          'Carpenter',
+          'Painter',
+          'Gardener',
+          'Actor',
+          'Police officer',
+          'Firefighter',
+          'Baker',
+          'Electrician'
+        ]
+      },
+      es: {
+        key: 'Profesión',
+        clues: [
+          'Trabajo',
+          'Empleo',
+          'Oficio',
+          'Salario',
+          'Médico',
+          'Profesor',
+          'Abogado',
+          'Ingeniero',
+          'Carpintero',
+          'Pintor',
+          'Jardinero',
+          'Actor',
+          'Policía',
+          'Bombero',
+          'Panadero',
+          'Electricista'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.5',
+    levelData: {
+      pt: {
+        key: 'Transportes',
+        clues: [
+          'Meio de locomoção',
+          'Carro',
+          'Ônibus',
+          'Avião',
+          'Barco',
+          'Trem',
+          'Bicicleta',
+          'Moto',
+          'Caminhão',
+          'Helicóptero',
+          'Cavalo',
+          'Transporte público',
+          'Marítimo',
+          'Aéreo',
+          'Terrestre',
+          'Rodovia'
+        ]
+      },
+      en: {
+        key: 'Transportation',
+        clues: [
+          'Mode of transportation',
+          'Car',
+          'Bus',
+          'Airplane',
+          'Boat',
+          'Train',
+          'Bicycle',
+          'Motorcycle',
+          'Truck',
+          'Helicopter',
+          'Horse',
+          'Public transportation',
+          'Maritime',
+          'Airborne',
+          'Land',
+          'Highway'
+        ]
+      },
+      es: {
+        key: 'Transportes',
+        clues: [
+          'Medio de locomoción',
+          'Coche',
+          'Autobús',
+          'Avión',
+          'Barco',
+          'Tren',
+          'Bicicleta',
+          'Motocicleta',
+          'Camión',
+          'Helicóptero',
+          'Caballo',
+          'Transporte público',
+          'Marítimo',
+          'Aéreo',
+          'Terrestre',
+          'Autopista'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.6',
+    levelData: {
+      pt: {
+        key: 'Alimentos',
+        clues: [
+          'Comida',
+          'Nutrição',
+          'Sabor',
+          'Refeição',
+          'Frutas',
+          'Vegetais',
+          'Carne',
+          'Peixe',
+          'Grãos',
+          'Laticínios',
+          'Proteína',
+          'Fibras',
+          'Vitaminas',
+          'Minerais',
+          'Açúcar',
+          'Gordura'
+        ]
+      },
+      en: {
+        key: 'Foods',
+        clues: [
+          'Food',
+          'Nutrition',
+          'Taste',
+          'Meal',
+          'Fruits',
+          'Vegetables',
+          'Meat',
+          'Fish',
+          'Grains',
+          'Dairy',
+          'Protein',
+          'Fiber',
+          'Vitamins',
+          'Minerals',
+          'Sugar',
+          'Fat'
+        ]
+      },
+      es: {
+        key: 'Alimentos',
+        clues: [
+          'Comida',
+          'Nutrición',
+          'Sabor',
+          'Comida',
+          'Frutas',
+          'Verduras',
+          'Carne',
+          'Pescado',
+          'Cereales',
+          'Productos lácteos',
+          'Proteína',
+          'Fibra',
+          'Vitaminas',
+          'Minerales',
+          'Azúcar',
+          'Grasa'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.7',
+    levelData: {
+      pt: {
+        key: 'Elementos da Natureza',
+        clues: [
+          'Água',
+          'Fogo',
+          'Terra',
+          'Ar',
+          'Planta',
+          'Pedra',
+          'Montanha',
+          'Céu',
+          'Floresta',
+          'Luz',
+          'Sombra',
+          'Nuvem',
+          'Estrela',
+          'Ventania',
+          'Chuva',
+          'Raio'
+        ]
+      },
+      en: {
+        key: 'Elements of Nature',
+        clues: [
+          'Water',
+          'Fire',
+          'Earth',
+          'Air',
+          'Plant',
+          'Stone',
+          'Mountain',
+          'Sky',
+          'Forest',
+          'Light',
+          'Shadow',
+          'Cloud',
+          'Star',
+          'Wind',
+          'Rain',
+          'Lightning'
+        ]
+      },
+      es: {
+        key: 'Elementos de la Naturaleza',
+        clues: [
+          'Agua',
+          'Fuego',
+          'Tierra',
+          'Aire',
+          'Planta',
+          'Piedra',
+          'Montaña',
+          'Cielo',
+          'Bosque',
+          'Luz',
+          'Sombra',
+          'Nube',
+          'Estrella',
+          'Viento',
+          'Lluvia',
+          'Rayo'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.8',
+    levelData: {
+      pt: {
+        key: 'Partes do Corpo',
+        clues: [
+          'Cabeça',
+          'Braço',
+          'Perna',
+          'Pé',
+          'Mão',
+          'Dedo',
+          'Olho',
+          'Orelha',
+          'Boca',
+          'Nariz',
+          'Cabelo',
+          'Cotovelo',
+          'Joelho',
+          'Costas',
+          'Dente',
+          'Língua'
+        ]
+      },
+      en: {
+        key: 'Body Parts',
+        clues: [
+          'Head',
+          'Arm',
+          'Leg',
+          'Foot',
+          'Hand',
+          'Finger',
+          'Eye',
+          'Ear',
+          'Mouth',
+          'Nose',
+          'Hair',
+          'Elbow',
+          'Knee',
+          'Back',
+          'Tooth',
+          'Tongue'
+        ]
+      },
+      es: {
+        key: 'Partes del Cuerpo',
+        clues: [
+          'Cabeza',
+          'Brazo',
+          'Pierna',
+          'Pie',
+          'Mano',
+          'Dedo',
+          'Ojo',
+          'Oreja',
+          'Boca',
+          'Nariz',
+          'Cabello',
+          'Codo',
+          'Rodilla',
+          'Espalda',
+          'Diente',
+          'Lengua'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.9',
+    levelData: {
+      pt: {
+        key: 'Dias da Semana',
+        clues: [
+          'Segunda-feira',
+          'Trabalho',
+          'Quarta-feira',
+          'Feriado',
+          'Sábado',
+          'Agenda',
+          'Terça-feira',
+          'Rotina',
+          'Domingo',
+          'Compromisso',
+          'Descanso',
+          'Quinta-feira',
+          'Fim de semana',
+          'Calendário',
+          'Sexta-feira',
+          'Dia'
+        ]
+      },
+      en: {
+        key: 'Days of the Week',
+        clues: [
+          'Monday',
+          'Work',
+          'Wednesday',
+          'Holiday',
+          'Saturday',
+          'Schedule',
+          'Tuesday',
+          'Routine',
+          'Sunday',
+          'Appointment',
+          'Rest',
+          'Thursday',
+          'Weekend',
+          'Calendar',
+          'Friday',
+          'Day'
+        ]
+      },
+      es: {
+        key: 'Días de la Semana',
+        clues: [
+          'Lunes',
+          'Trabajo',
+          'Miércoles',
+          'Feriado',
+          'Sábado',
+          'Agenda',
+          'Martes',
+          'Rutina',
+          'Domingo',
+          'Compromiso',
+          'Descanso',
+          'Jueves',
+          'Fin de semana',
+          'Calendario',
+          'Viernes',
+          'Día'
+        ]
+      }
+    }
+  },
+  {
+    id: '1.10',
+    levelData: {
+      pt: {
+        key: 'Comunicação',
+        clues: [
+          'Telefone',
+          'Internet',
+          'Jornal',
+          'Rádio',
+          'TV',
+          'Mensagem',
+          'Redes sociais',
+          'Carta',
+          'E-mail',
+          'Revista',
+          'Notícia',
+          'Comunicado',
+          'Informação',
+          'Televisão',
+          'Comunicador',
+          'Mídia'
+        ]
+      },
+      en: {
+        key: 'Communication',
+        clues: [
+          'Telephone',
+          'Internet',
+          'Newspaper',
+          'Radio',
+          'TV',
+          'Message',
+          'Social media',
+          'Letter',
+          'Email',
+          'Magazine',
+          'News',
+          'Bulletin',
+          'Information',
+          'Television',
+          'Communicator',
+          'Media'
+        ]
+      },
+      es: {
+        key: 'Comunicación',
+        clues: [
+          'Teléfono',
+          'Internet',
+          'Periódico',
+          'Radio',
+          'TV',
+          'Mensaje',
+          'Redes sociales',
+          'Carta',
+          'Correo electrónico',
+          'Revista',
+          'Noticia',
+          'Boletín',
+          'Información',
+          'Televisión',
+          'Comunicador',
+          'Medios'
         ]
       }
     }
