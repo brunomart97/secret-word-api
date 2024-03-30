7dceac8effa3a0ef941cf990d7f584e6eebe63d3
Bruno Martins <brunomart97@hotmail.com>
2024-03-30 10:44:38 -0300

    chore: level 4 clues

--- src/data/levels/level4.ts
+++ src/data/levels/level4.ts
@@ -7,20 +7,20 @@ export const level4 = [
         clues: [
           'Hidrogênio',
           'Hélio',
+          'Lítio',
+          'Berílio',
+          'Boro',
           'Carbono',
-          'Oxigênio',
           'Nitrogênio',
+          'Oxigênio',
+          'Flúor',
+          'Neônio',
           'Sódio',
           'Magnésio',
-          'Ferro',
-          'Cobre',
-          'Zinco',
-          'Prata',
-          'Iodo',
-          'Ouro',
-          'Mercúrio',
-          'Urânio',
-          'Plutônio'
+          'Alumínio',
+          'Silício',
+          'Fósforo',
+          'Enxofre'
         ]
       },
       en: {
@@ -28,20 +28,20 @@ export const level4 = [
         clues: [
           'Hydrogen',
           'Helium',
+          'Lithium',
+          'Beryllium',
+          'Boron',
           'Carbon',
-          'Oxygen',
           'Nitrogen',
+          'Oxygen',
+          'Fluorine',
+          'Neon',
           'Sodium',
           'Magnesium',
-          'Iron',
-          'Copper',
-          'Zinc',
-          'Silver',
-          'Iodine',
-          'Gold',
-          'Mercury',
-          'Uranium',
-          'Plutonium'
+          'Aluminum',
+          'Silicon',
+          'Phosphorus',
+          'Sulfur'
         ]
       },
       es: {
@@ -49,20 +49,20 @@ export const level4 = [
         clues: [
           'Hidrógeno',
           'Helio',
+          'Litio',
+          'Berilio',
+          'Boro',
           'Carbono',
-          'Oxígeno',
           'Nitrógeno',
+          'Oxígeno',
+          'Flúor',
+          'Neón',
           'Sodio',
           'Magnesio',
-          'Hierro',
-          'Cobre',
-          'Zinc',
-          'Plata',
-          'Yodo',
-          'Oro',
-          'Mercurio',
-          'Uranio',
-          'Plutonio'
+          'Aluminio',
+          'Silicio',
+          'Fósforo',
+          'Azufre'
         ]
       }
     }
@@ -71,66 +71,610 @@ export const level4 = [
     id: '4.2',
     levelData: {
       pt: {
-        key: 'Sistema Solar',
+        key: 'Computador',
         clues: [
-          'Sol',
-          'Mercúrio',
-          'Vênus',
-          'Terra',
-          'Marte',
-          'Júpiter',
-          'Saturno',
-          'Urano',
-          'Netuno',
-          'Plutão',
-          'Lua',
-          'Cometa',
-          'Asteróide',
-          'Meteorito',
-          'Estrela',
-          'Planeta Anão'
+          'Teclado',
+          'Mouse',
+          'Monitor',
+          'Processador',
+          'Memória',
+          'Disco rígido',
+          'Software',
+          'Hardware',
+          'Internet',
+          'Rede',
+          'Mousepad',
+          'Teclado numérico',
+          'Placa-mãe',
+          'USB',
+          'Windows',
+          'Mac'
         ]
       },
       en: {
-        key: 'Solar System',
+        key: 'Computer',
         clues: [
-          'Sun',
-          'Mercury',
-          'Venus',
-          'Earth',
-          'Mars',
-          'Jupiter',
-          'Saturn',
-          'Uranus',
-          'Neptune',
-          'Pluto',
-          'Moon',
-          'Comet',
-          'Asteroid',
-          'Meteorite',
-          'Star',
-          'Dwarf Planet'
+          'Keyboard',
+          'Mouse',
+          'Monitor',
+          'Processor',
+          'Memory',
+          'Hard drive',
+          'Software',
+          'Hardware',
+          'Internet',
+          'Network',
+          'Mouse pad',
+          'Numeric keypad',
+          'Motherboard',
+          'USB',
+          'Windows',
+          'Mac'
         ]
       },
       es: {
-        key: 'Sistema Solar',
+        key: 'Computadora',
         clues: [
-          'Sol',
-          'Mercurio',
-          'Venus',
-          'Tierra',
-          'Marte',
-          'Júpiter',
-          'Saturno',
-          'Urano',
-          'Neptuno',
-          'Plutón',
-          'Luna',
-          'Cometa',
-          'Asteroides',
-          'Meteorito',
-          'Estrella',
-          'Planeta Enano'
+          'Teclado',
+          'Ratón',
+          'Monitor',
+          'Procesador',
+          'Memoria',
+          'Disco duro',
+          'Software',
+          'Hardware',
+          'Internet',
+          'Red',
+          'Alfombrilla de ratón',
+          'Teclado numérico',
+          'Placa base',
+          'USB',
+          'Windows',
+          'Mac'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.3',
+    levelData: {
+      pt: {
+        key: 'Fotografia',
+        clues: [
+          'Câmera',
+          'Lente',
+          'Foco',
+          'Obturador',
+          'Flash',
+          'Exposição',
+          'ISO',
+          'Diafragma',
+          'Composição',
+          'Retrato',
+          'Paisagem',
+          'Preto e branco',
+          'Fotógrafo',
+          'Enquadramento',
+          'Cenário',
+          'Digital'
+        ]
+      },
+      en: {
+        key: 'Photography',
+        clues: [
+          'Camera',
+          'Lens',
+          'Focus',
+          'Shutter',
+          'Flash',
+          'Exposure',
+          'ISO',
+          'Aperture',
+          'Composition',
+          'Portrait',
+          'Landscape',
+          'Black and white',
+          'Photographer',
+          'Frame',
+          'Scene',
+          'Digital'
+        ]
+      },
+      es: {
+        key: 'Fotografía',
+        clues: [
+          'Cámara',
+          'Lente',
+          'Enfoque',
+          'Obturador',
+          'Flash',
+          'Exposición',
+          'ISO',
+          'Diafragma',
+          'Composición',
+          'Retrato',
+          'Paisaje',
+          'Blanco y negro',
+          'Fotógrafo',
+          'Encuadre',
+          'Escena',
+          'Digital'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.4',
+    levelData: {
+      pt: {
+        key: 'Montanha',
+        clues: [
+          'Pico',
+          'Cordilheira',
+          'Altitude',
+          'Trilha',
+          'Escalar',
+          'Neve',
+          'Geleira',
+          'Aventura',
+          'Vista panorâmica',
+          'Cume',
+          'Exploração',
+          'Natureza',
+          'Rocha',
+          'Vale',
+          'Alpinismo',
+          'Cachoeira'
+        ]
+      },
+      en: {
+        key: 'Mountain',
+        clues: [
+          'Peak',
+          'Mountain range',
+          'Altitude',
+          'Trail',
+          'Climbing',
+          'Snow',
+          'Glacier',
+          'Adventure',
+          'Scenic view',
+          'Summit',
+          'Exploration',
+          'Nature',
+          'Rock',
+          'Valley',
+          'Mountaineering',
+          'Waterfall'
+        ]
+      },
+      es: {
+        key: 'Montaña',
+        clues: [
+          'Pico',
+          'Cordillera',
+          'Altitud',
+          'Sendero',
+          'Escalada',
+          'Nieve',
+          'Glaciar',
+          'Aventura',
+          'Vista panorámica',
+          'Cumbre',
+          'Exploración',
+          'Naturaleza',
+          'Roca',
+          'Valle',
+          'Alpinismo',
+          'Cascada'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.5',
+    levelData: {
+      pt: {
+        key: 'Chocolate',
+        clues: [
+          'Doce',
+          'Cacau',
+          'Branco',
+          'Amargo',
+          'Ao leite',
+          'Tablete',
+          'Barra',
+          'Trufa',
+          'Cremoso',
+          'Sabor',
+          'Aroma',
+          'Delicioso',
+          'Fondue',
+          'Confeito',
+          'Derretido',
+          'Bebida'
+        ]
+      },
+      en: {
+        key: 'Chocolate',
+        clues: [
+          'Sweet',
+          'Cocoa',
+          'White',
+          'Bitter',
+          'Milk',
+          'Bar',
+          'Block',
+          'Truffle',
+          'Creamy',
+          'Flavor',
+          'Aroma',
+          'Delicious',
+          'Fondue',
+          'Topping',
+          'Melted',
+          'Beverage'
+        ]
+      },
+      es: {
+        key: 'Chocolate',
+        clues: [
+          'Dulce',
+          'Cacao',
+          'Blanco',
+          'Amargo',
+          'Con leche',
+          'Tableta',
+          'Barra',
+          'Trufa',
+          'Cremoso',
+          'Sabor',
+          'Aroma',
+          'Delicioso',
+          'Fondue',
+          'Cobertura',
+          'Derretido',
+          'Bebida'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.6',
+    levelData: {
+      pt: {
+        key: 'Arquitetura',
+        clues: [
+          'Projeto',
+          'Construção',
+          'Edifício',
+          'Design',
+          'Estilo',
+          'Arco',
+          'Coluna',
+          'Fachada',
+          'Pilar',
+          'Janela',
+          'Porta',
+          'Telhado',
+          'Ponte',
+          'Torre',
+          'Pátio',
+          'Urbanismo'
+        ]
+      },
+      en: {
+        key: 'Architecture',
+        clues: [
+          'Project',
+          'Construction',
+          'Building',
+          'Design',
+          'Style',
+          'Arch',
+          'Column',
+          'Facade',
+          'Pillar',
+          'Window',
+          'Door',
+          'Roof',
+          'Bridge',
+          'Tower',
+          'Courtyard',
+          'Urban planning'
+        ]
+      },
+      es: {
+        key: 'Arquitectura',
+        clues: [
+          'Proyecto',
+          'Construcción',
+          'Edificio',
+          'Diseño',
+          'Estilo',
+          'Arco',
+          'Columna',
+          'Fachada',
+          'Pilar',
+          'Ventana',
+          'Puerta',
+          'Tejado',
+          'Puente',
+          'Torre',
+          'Patio',
+          'Urbanismo'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.7',
+    levelData: {
+      pt: {
+        key: 'Dinossauro',
+        clues: [
+          'Jurássico',
+          'Extinto',
+          'Fóssil',
+          'Réptil',
+          'Carnívoro',
+          'Herbívoro',
+          'Era',
+          'Dente',
+          'Presa',
+          'Ossos',
+          'Cauda',
+          'Gigante',
+          'Pescoço longo',
+          'Jurassic Park',
+          'Paleontologia',
+          'Evolução'
+        ]
+      },
+      en: {
+        key: 'Dinosaur',
+        clues: [
+          'Jurassic',
+          'Extinct',
+          'Fossil',
+          'Reptile',
+          'Carnivore',
+          'Herbivore',
+          'Era',
+          'Tooth',
+          'Prey',
+          'Bones',
+          'Tail',
+          'Giant',
+          'Long neck',
+          'Jurassic Park',
+          'Paleontology',
+          'Evolution'
+        ]
+      },
+      es: {
+        key: 'Dinosaurio',
+        clues: [
+          'Jurásico',
+          'Extinto',
+          'Fósil',
+          'Reptil',
+          'Carnívoro',
+          'Herbívoro',
+          'Era',
+          'Diente',
+          'Presas',
+          'Huesos',
+          'Cola',
+          'Gigante',
+          'Cuello largo',
+          'Jurassic Park',
+          'Paleontología',
+          'Evolución'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.8',
+    levelData: {
+      pt: {
+        key: 'Navio',
+        clues: [
+          'Mar',
+          'Embarcação',
+          'Viagem',
+          'Porto',
+          'Navegação',
+          'Tripulação',
+          'Comandante',
+          'Casco',
+          'Mastro',
+          'Vela',
+          'Âncora',
+          'Leme',
+          'Porta-contêineres',
+          'Cruzeiro',
+          'Carga',
+          'Bússola'
+        ]
+      },
+      en: {
+        key: 'Ship',
+        clues: [
+          'Sea',
+          'Vessel',
+          'Journey',
+          'Port',
+          'Navigation',
+          'Crew',
+          'Captain',
+          'Hull',
+          'Mast',
+          'Sail',
+          'Anchor',
+          'Rudder',
+          'Container ship',
+          'Cruise',
+          'Cargo',
+          'Compass'
+        ]
+      },
+      es: {
+        key: 'Barco',
+        clues: [
+          'Mar',
+          'Buque',
+          'Viaje',
+          'Puerto',
+          'Navegación',
+          'Tripulación',
+          'Capitán',
+          'Casco',
+          'Mástil',
+          'Vela',
+          'Ancla',
+          'Timón',
+          'Portacontenedores',
+          'Crucero',
+          'Carga',
+          'Brújula'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.9',
+    levelData: {
+      pt: {
+        key: 'Avião',
+        clues: [
+          'Aeroplano',
+          'Asa',
+          'Piloto',
+          'Céu',
+          'Voo',
+          'Cabine',
+          'Motor',
+          'Aeroporto',
+          'Passageiro',
+          'Tripulação',
+          'Decolagem',
+          'Aterrissagem',
+          'Turbina',
+          'Fuselagem',
+          'Janela',
+          'Controle'
+        ]
+      },
+      en: {
+        key: 'Airplane',
+        clues: [
+          'Aeroplane',
+          'Wing',
+          'Pilot',
+          'Sky',
+          'Flight',
+          'Cabin',
+          'Engine',
+          'Airport',
+          'Passenger',
+          'Crew',
+          'Takeoff',
+          'Landing',
+          'Turbine',
+          'Fuselage',
+          'Window',
+          'Control'
+        ]
+      },
+      es: {
+        key: 'Avión',
+        clues: [
+          'Aeroplano',
+          'Ala',
+          'Piloto',
+          'Cielo',
+          'Vuelo',
+          'Cabina',
+          'Motor',
+          'Aeropuerto',
+          'Pasajero',
+          'Tripulación',
+          'Despegue',
+          'Aterrizaje',
+          'Turbina',
+          'Fuselaje',
+          'Ventana',
+          'Control'
+        ]
+      }
+    }
+  },
+  {
+    id: '4.10',
+    levelData: {
+      pt: {
+        key: 'Cinema',
+        clues: [
+          'Filme',
+          'Diretor',
+          'Atores',
+          'Atrizes',
+          'Roteiro',
+          'Câmera',
+          'Cenário',
+          'Trilha sonora',
+          'Efeitos especiais',
+          'Gênero',
+          'Festival',
+          'Premiação',
+          'Estreia',
+          'Bilheteria',
+          'Fotografia',
+          'Oscar'
+        ]
+      },
+      en: {
+        key: 'Cinema',
+        clues: [
+          'Movie',
+          'Director',
+          'Actors',
+          'Actresses',
+          'Script',
+          'Camera',
+          'Set',
+          'Soundtrack',
+          'Special effects',
+          'Genre',
+          'Festival',
+          'Award',
+          'Premiere',
+          'Box office',
+          'Cinematography',
+          'Oscar'
+        ]
+      },
+      es: {
+        key: 'Cine',
+        clues: [
+          'Película',
+          'Director',
+          'Actores',
+          'Actrices',
+          'Guión',
+          'Cámara',
+          'Escenario',
+          'Banda sonora',
+          'Efectos especiales',
+          'Género',
+          'Festival',
+          'Premio',
+          'Estreno',
+          'Taquilla',
+          'Fotografía',
+          'Oscar'
         ]
       }
     }
