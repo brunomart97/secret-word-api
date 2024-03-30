ffb32ac55e615a9191fa78adfcc75dabad518d91
Bruno Martins <brunomart97@hotmail.com>
2024-03-30 11:09:02 -0300

    chore: level 6 clues

--- src/data/levels/level6.ts
+++ src/data/levels/level6.ts
@@ -3,66 +3,678 @@ export const level6 = [
     id: '6.1',
     levelData: {
       pt: {
-        key: 'Exploração Espacial',
+        key: 'Religião',
         clues: [
-          'Astronauta',
-          'Foguete',
-          'Estação Espacial',
-          'Órbita',
-          'Telescópio',
-          'Gravidade',
-          'Satélite',
-          'Terra',
-          'Lua',
-          'Planeta',
-          'Pesquisa Científica',
-          'Agência Espacial',
-          'Alienígena',
-          'Astrofísica',
-          'Viagem Interplanetária',
-          'Colonização'
+          'Crença',
+          'Devoção',
+          'Espiritualidade',
+          'Deus',
+          'Sagrado',
+          'Ritual',
+          'Fé',
+          'Templo',
+          'Sacerdote',
+          'Santo',
+          'Oração',
+          'Sacramento',
+          'Divindade',
+          'Adoração',
+          'Profeta',
+          'Dogma'
         ]
       },
       en: {
-        key: 'Space Exploration',
+        key: 'Religion',
         clues: [
-          'Astronaut',
-          'Rocket',
-          'Space Station',
-          'Orbit',
-          'Telescope',
-          'Gravity',
-          'Satellite',
-          'Earth',
-          'Moon',
-          'Planet',
-          'Scientific Research',
-          'Space Agency',
-          'Alien',
-          'Astrophysics',
-          'Interplanetary Travel',
-          'Colonization'
+          'Belief',
+          'Devotion',
+          'Spirituality',
+          'God',
+          'Sacred',
+          'Ritual',
+          'Faith',
+          'Temple',
+          'Priest',
+          'Saint',
+          'Prayer',
+          'Sacrament',
+          'Divinity',
+          'Worship',
+          'Prophet',
+          'Dogma'
         ]
       },
       es: {
-        key: 'Exploración Espacial',
+        key: 'Religión',
         clues: [
-          'Astronauta',
-          'Cohete',
-          'Estación Espacial',
-          'Órbita',
-          'Telescopio',
-          'Gravedad',
-          'Satélite',
-          'Tierra',
-          'Luna',
-          'Planeta',
-          'Investigación Científica',
-          'Agencia Espacial',
-          'Extraterrestre',
-          'Astrofísica',
-          'Viaje Interplanetario',
-          'Colonización'
+          'Creencia',
+          'Devoción',
+          'Espiritualidad',
+          'Dios',
+          'Sagrado',
+          'Ritual',
+          'Fe',
+          'Templo',
+          'Sacerdote',
+          'Santo',
+          'Oración',
+          'Sacramento',
+          'Divinidad',
+          'Adoración',
+          'Profeta',
+          'Dogma'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.2',
+    levelData: {
+      pt: {
+        key: 'Meio Ambiente',
+        clues: [
+          'Natureza',
+          'Ecossistema',
+          'Sustentabilidade',
+          'Biodiversidade',
+          'Preservação',
+          'Poluição',
+          'Reciclagem',
+          'Desmatamento',
+          'Aquecimento global',
+          'Efeito estufa',
+          'Conscientização',
+          'Resíduos',
+          'Renovável',
+          'Água',
+          'Ar',
+          'Solo'
+        ]
+      },
+      en: {
+        key: 'Environment',
+        clues: [
+          'Nature',
+          'Ecosystem',
+          'Sustainability',
+          'Biodiversity',
+          'Preservation',
+          'Pollution',
+          'Recycling',
+          'Deforestation',
+          'Global warming',
+          'Greenhouse effect',
+          'Awareness',
+          'Waste',
+          'Renewable',
+          'Water',
+          'Air',
+          'Soil'
+        ]
+      },
+      es: {
+        key: 'Medio Ambiente',
+        clues: [
+          'Naturaleza',
+          'Ecosistema',
+          'Sostenibilidad',
+          'Biodiversidad',
+          'Preservación',
+          'Contaminación',
+          'Reciclaje',
+          'Deforestación',
+          'Calentamiento global',
+          'Efecto invernadero',
+          'Concienciación',
+          'Residuos',
+          'Renovable',
+          'Agua',
+          'Aire',
+          'Suelo'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.3',
+    levelData: {
+      pt: {
+        key: 'Política',
+        clues: [
+          'Governo',
+          'Democracia',
+          'Partido',
+          'Eleição',
+          'Legislação',
+          'Corrupção',
+          'Liderança',
+          'Poder',
+          'Ideologia',
+          'Cidadania',
+          'Constituição',
+          'Campanha',
+          'Debate',
+          'Legislativo',
+          'Executivo',
+          'Judiciário'
+        ]
+      },
+      en: {
+        key: 'Politics',
+        clues: [
+          'Government',
+          'Democracy',
+          'Party',
+          'Election',
+          'Legislation',
+          'Corruption',
+          'Leadership',
+          'Power',
+          'Ideology',
+          'Citizenship',
+          'Constitution',
+          'Campaign',
+          'Debate',
+          'Legislative',
+          'Executive',
+          'Judiciary'
+        ]
+      },
+      es: {
+        key: 'Política',
+        clues: [
+          'Gobierno',
+          'Democracia',
+          'Partido',
+          'Elección',
+          'Legislación',
+          'Corrupción',
+          'Liderazgo',
+          'Poder',
+          'Ideología',
+          'Ciudadanía',
+          'Constitución',
+          'Campaña',
+          'Debate',
+          'Legislativo',
+          'Ejecutivo',
+          'Judicial'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.4',
+    levelData: {
+      pt: {
+        key: 'Arte',
+        clues: [
+          'Expressão',
+          'Criatividade',
+          'Beleza',
+          'Estética',
+          'Inspiração',
+          'Cultura',
+          'Pintura',
+          'Escultura',
+          'Música',
+          'Dança',
+          'Teatro',
+          'Literatura',
+          'Arquitetura',
+          'Design',
+          'Performance',
+          'Artes plásticas'
+        ]
+      },
+      en: {
+        key: 'Art',
+        clues: [
+          'Expression',
+          'Creativity',
+          'Beauty',
+          'Aesthetics',
+          'Inspiration',
+          'Culture',
+          'Painting',
+          'Sculpture',
+          'Music',
+          'Dance',
+          'Theater',
+          'Literature',
+          'Architecture',
+          'Design',
+          'Performance',
+          'Visual arts'
+        ]
+      },
+      es: {
+        key: 'Arte',
+        clues: [
+          'Expresión',
+          'Creatividad',
+          'Belleza',
+          'Estética',
+          'Inspiración',
+          'Cultura',
+          'Pintura',
+          'Escultura',
+          'Música',
+          'Danza',
+          'Teatro',
+          'Literatura',
+          'Arquitectura',
+          'Diseño',
+          'Actuación',
+          'Artes visuales'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.5',
+    levelData: {
+      pt: {
+        key: 'Culinária',
+        clues: [
+          'Gastronomia',
+          'Receita',
+          'Cozinha',
+          'Chef',
+          'Ingredientes',
+          'Prato',
+          'Sabor',
+          'Tempero',
+          'Cultura',
+          'Tradição',
+          'Nutrição',
+          'Sobremesa',
+          'Especiaria',
+          'Cozinhar',
+          'Gourmet',
+          'Refeição'
+        ]
+      },
+      en: {
+        key: 'Cuisine',
+        clues: [
+          'Gastronomy',
+          'Recipe',
+          'Kitchen',
+          'Chef',
+          'Ingredients',
+          'Dish',
+          'Flavor',
+          'Seasoning',
+          'Culture',
+          'Tradition',
+          'Nutrition',
+          'Dessert',
+          'Spice',
+          'Cooking',
+          'Gourmet',
+          'Meal'
+        ]
+      },
+      es: {
+        key: 'Cocina',
+        clues: [
+          'Gastronomía',
+          'Receta',
+          'Cocina',
+          'Chef',
+          'Ingredientes',
+          'Plato',
+          'Sabor',
+          'Condimento',
+          'Cultura',
+          'Tradición',
+          'Nutrición',
+          'Postre',
+          'Especia',
+          'Cocinar',
+          'Gourmet',
+          'Comida'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.6',
+    levelData: {
+      pt: {
+        key: 'Conflito',
+        clues: [
+          'Guerra',
+          'Violência',
+          'Tensão',
+          'Disputa',
+          'Confronto',
+          'Hostilidade',
+          'Batalha',
+          'Conflito armado',
+          'Paz',
+          'Resolução',
+          'Negociação',
+          'Mediador',
+          'Vítima',
+          'Causa',
+          'Impacto',
+          'Consequência'
+        ]
+      },
+      en: {
+        key: 'Conflict',
+        clues: [
+          'War',
+          'Violence',
+          'Tension',
+          'Dispute',
+          'Confrontation',
+          'Hostility',
+          'Battle',
+          'Armed conflict',
+          'Peace',
+          'Resolution',
+          'Negotiation',
+          'Mediator',
+          'Victim',
+          'Cause',
+          'Impact',
+          'Consequence'
+        ]
+      },
+      es: {
+        key: 'Conflicto',
+        clues: [
+          'Guerra',
+          'Violencia',
+          'Tensión',
+          'Disputa',
+          'Confrontación',
+          'Hostilidad',
+          'Batalla',
+          'Conflicto armado',
+          'Paz',
+          'Resolución',
+          'Negociación',
+          'Mediador',
+          'Víctima',
+          'Causa',
+          'Impacto',
+          'Consecuencia'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.7',
+    levelData: {
+      pt: {
+        key: 'Ciência',
+        clues: [
+          'Pesquisa',
+          'Experimento',
+          'Descoberta',
+          'Teoria',
+          'Hipótese',
+          'Método científico',
+          'Laboratório',
+          'Inovação',
+          'Tecnologia',
+          'Conhecimento',
+          'Biologia',
+          'Química',
+          'Física',
+          'Matemática',
+          'Astronomia',
+          'Geologia'
+        ]
+      },
+      en: {
+        key: 'Science',
+        clues: [
+          'Research',
+          'Experiment',
+          'Discovery',
+          'Theory',
+          'Hypothesis',
+          'Scientific method',
+          'Laboratory',
+          'Innovation',
+          'Technology',
+          'Knowledge',
+          'Biology',
+          'Chemistry',
+          'Physics',
+          'Mathematics',
+          'Astronomy',
+          'Geology'
+        ]
+      },
+      es: {
+        key: 'Ciencia',
+        clues: [
+          'Investigación',
+          'Experimento',
+          'Descubrimiento',
+          'Teoría',
+          'Hipótesis',
+          'Método científico',
+          'Laboratorio',
+          'Innovación',
+          'Tecnología',
+          'Conocimiento',
+          'Biología',
+          'Química',
+          'Física',
+          'Matemáticas',
+          'Astronomía',
+          'Geología'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.8',
+    levelData: {
+      pt: {
+        key: 'Entretenimento',
+        clues: [
+          'Diversão',
+          'Cultura',
+          'Arte',
+          'Música',
+          'Cinema',
+          'Teatro',
+          'Televisão',
+          'Jogos',
+          'Esporte',
+          'Show',
+          'Festa',
+          'Parque de diversões',
+          'Circo',
+          'Comédia',
+          'Drama',
+          'Aventura'
+        ]
+      },
+      en: {
+        key: 'Entertainment',
+        clues: [
+          'Fun',
+          'Culture',
+          'Art',
+          'Music',
+          'Cinema',
+          'Theater',
+          'Television',
+          'Games',
+          'Sports',
+          'Show',
+          'Party',
+          'Amusement park',
+          'Circus',
+          'Comedy',
+          'Drama',
+          'Adventure'
+        ]
+      },
+      es: {
+        key: 'Entretenimiento',
+        clues: [
+          'Diversión',
+          'Cultura',
+          'Arte',
+          'Música',
+          'Cine',
+          'Teatro',
+          'Televisión',
+          'Juegos',
+          'Deportes',
+          'Espectáculo',
+          'Fiesta',
+          'Parque de atracciones',
+          'Circo',
+          'Comedia',
+          'Drama',
+          'Aventura'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.9',
+    levelData: {
+      pt: {
+        key: 'Literatura',
+        clues: [
+          'Livro',
+          'Escritor',
+          'Autor',
+          'Poesia',
+          'Romance',
+          'Conto',
+          'Drama',
+          'Ficção',
+          'Não ficção',
+          'Clássico',
+          'Best-seller',
+          'Editora',
+          'Biblioteca',
+          'Leitura',
+          'Personagem',
+          'Enredo'
+        ]
+      },
+      en: {
+        key: 'Literature',
+        clues: [
+          'Book',
+          'Writer',
+          'Author',
+          'Poetry',
+          'Novel',
+          'Short story',
+          'Drama',
+          'Fiction',
+          'Non-fiction',
+          'Classic',
+          'Bestseller',
+          'Publisher',
+          'Library',
+          'Reading',
+          'Character',
+          'Plot'
+        ]
+      },
+      es: {
+        key: 'Literatura',
+        clues: [
+          'Libro',
+          'Escritor',
+          'Autor',
+          'Poesía',
+          'Novela',
+          'Cuento',
+          'Drama',
+          'Ficción',
+          'No ficción',
+          'Clásico',
+          'Best-seller',
+          'Editorial',
+          'Biblioteca',
+          'Lectura',
+          'Personaje',
+          'Trama'
+        ]
+      }
+    }
+  },
+  {
+    id: '6.10',
+    levelData: {
+      pt: {
+        key: 'Skate',
+        clues: [
+          'Esporte',
+          'Prancha',
+          'Rodas',
+          'Manobra',
+          'Ollie',
+          'Truque',
+          'Half-pipe',
+          'Street',
+          'Rampa',
+          'Aéreo',
+          'Grip',
+          'Deslizar',
+          'Radical',
+          'Competição',
+          'Skatista',
+          'Cultura'
+        ]
+      },
+      en: {
+        key: 'Skateboarding',
+        clues: [
+          'Sport',
+          'Board',
+          'Wheels',
+          'Trick',
+          'Ollie',
+          'Stunt',
+          'Half-pipe',
+          'Street',
+          'Ramp',
+          'Air',
+          'Grip tape',
+          'Slide',
+          'Radical',
+          'Competition',
+          'Skater',
+          'Culture'
+        ]
+      },
+      es: {
+        key: 'Patineta',
+        clues: [
+          'Deporte',
+          'Tabla',
+          'Ruedas',
+          'Truco',
+          'Ollie',
+          'Acrobacia',
+          'Medio tubo',
+          'Calle',
+          'Rampa',
+          'Aire',
+          'Cinta de agarre',
+          'Deslizarse',
+          'Radical',
+          'Competición',
+          'Patinador',
+          'Cultura'
         ]
       }
     }
